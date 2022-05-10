1a4a37d72969531aaa48233f1a99a8d058820bcd
jms <bde574786@naver.com>
2022-05-10 10:49:39 +0900

    서브쿼리, join문 테스트

--- class_kha11_ex.sql
+++ class_kha11_ex.sql
@@ -17,13 +17,13 @@ where t.title = 'Staff'
 and t.to_date = '9999-01-01';
 
 -- 서브쿼리 
-select *
-from employees as e
-where e.emp_no in(select emp_no
-					from titles
-                    where title = 'Senior Engineer'
-                    and to_date = '9999-01-01');
-                    
+select e.*, t.title
+from employees as e, (select * from titles where title = 'Senior Engineer'
+                    and to_date = '9999-01-01') as t
+where e.emp_no = t.emp_no;
+
+-- -------------------------------------------------------------------
+            
 -- 사원번호로 이름과 직급, 연봉 조회하기 
 -- join
 select e.first_name, e.last_name, t.title, s.salary
@@ -36,6 +36,22 @@ where e.emp_no = 10007
 and s.to_date = '9999-01-01'
 group by s.emp_no;
 
+-- 서브쿼리
+select e.emp_no, e.first_name, e.last_name, t.title,
+		(select salary 
+        from salaries as s 
+        where e.emp_no = s.emp_no
+        and to_date = '9999-01-01'
+        group by emp_no) as salary
+from employees as e, (select * 
+						from titles as t 
+                        where t.emp_no = 10007 
+                        and t.to_date = '9999-01-01') as t
+where e.emp_no = t.emp_no;
+
+
+-- -------------------------------------------------------------
+
 -- 부서별 직원 조회하기 
 -- join
 select *
@@ -43,6 +59,15 @@ from employees as e
 inner join dept_emp as d
 on e.emp_no = d.emp_no
 where d.dept_no = 'd007';
+
+-- 서브쿼리 
+select *
+from employees as e, (select * 
+						from dept_emp
+                        where dept_no = 'd007') as d
+where e.emp_no = d.emp_no;
+
+-- ---------------------------------------------------------------
 
 -- 입사날짜별 직원 조회하기 
 -- join
@@ -52,6 +77,15 @@ inner join dept_emp as d
 on e.emp_no = d.emp_no
 where d.from_date > '1995-01-01';
 
+-- 서브쿼리 
+select *
+from employees as e, (select *
+						from dept_emp
+                        where from_date > '1985-01-01') as d
+where e.emp_no = d.emp_no;
+
+-- ----------------------------------------------------------------
+
 -- 부서별 현재 매니저 정보 조회하기
 -- join
 select *
@@ -60,3 +94,11 @@ inner join dept_manager as d
 on e.emp_no = d.emp_no
 where d.dept_no = 'd005'
 and d.to_date = '9999-01-01';
+
+-- 서브쿼리
+select *
+from employees as e
+where e.emp_no in(select emp_no
+					from dept_manager 
+                    where dept_no = 'd004'
+                    and to_date = '9999-01-01');
