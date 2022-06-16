c7fb850c16a32024be6d0479d033b8dba6c4bb7f
khyunah <89136556+khyunah@users.noreply.github.com>
2022-06-16 17:39:07 +0900

    회원가입 테스트

---
+++ jointest.sql
@@ -0,0 +1,13 @@
+create database movietest;
+use movietest;
+
+select * from actor;
+select * from movie;
+desc movie;
+select * from role;
+select * from score;
+
+-- -------------------------------
+
+select * from user;
+desc user;
