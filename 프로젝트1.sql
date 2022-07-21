c6f6313269a25b7aa4f79237c3ed21deb6d63627
khyunah <yd3085@naver.com>
2022-07-21 17:45:06 +0900

    Update 프로젝트1.sql

--- 프로젝트1.sql
+++ 프로젝트1.sql
@@ -9,14 +9,17 @@ select * from communityBoard;
 select * from communityLike;
 select * from reply;
 desc reply;
-desc board;
+desc communityBoard;
 desc user;
 desc basket;
 
+-- 쿼리문으로 데이터 입력할때 자동으로 시간 들어갈 수 있게 
+alter table communityBoard modify createDate datetime default CURRENT_TIMESTAMP;
+alter table user modify createDate datetime default CURRENT_TIMESTAMP;
 
 SELECT * FROM user WHERE username = "aa";
 
 insert into communityBoard (content, imageUrl, itemLink, originImageTitle, reaction, likeCount, title, userId)
-values('본문내용 1', '이미지주소', '아이템주소', '이미지 원본 이름', 0, 0, '제목9', 1);
+values('본문9', '이미지주소', '아이템주소', '이미지 원본 이름', 0, 0, '제목9', 1);
 insert into communityBoard (content, imageUrl, itemLink, originImageTitle, reaction, likeCount, title, userId)
-values('본문내용 2', '이미지주소2', '아이템주소2', '이미지 원본 이름2', 0, 0, '제목10', 1);
+values('본문10', '이미지주소2', '아이템주소2', '이미지 원본 이름2', 0, 0, '제목10', 1);
